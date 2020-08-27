b431122e13561ec8bb3c675d801ebc39de450198
union <unionolivia@gmail.com>
2020-08-27 01:56:04 +0100

    first commit

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -18,10 +18,8 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind([
-            UserRepositoryInterface::class => UserRepository::class,
-            ShowtimeRepositoryInterface::class => ShowtimeRepository::class
-            ]);
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(ShowtimeRepositoryInterface::class, ShowtimeRepository::class);
     }
 
     /**
